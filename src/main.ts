432611a46d018ad31ad7027932b6453ceffb782e
mitoma <mutetheradio@gmail.com>
2019-10-22 10:31:00 +0900

    ロケールを日本語に設定、ゴミ箱とズームボタン追加。

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import * as Blockly from 'blockly';
-import JA from 'blockly/msg/ja';
+import JA from 'blockly/msg/ja.js';
+
+Blockly.setLocale(JA);
 
 const menuXml = `<xml xmlns="https://developers.google.com/blockly/xml" id="toolbox" style="display: none">
                    <block type="controls_if"></block>
@@ -13,11 +15,19 @@ const menuXml = `<xml xmlns="https://developers.google.com/blockly/xml" id="tool
                    <block type="text_print"></block>
                  </xml>`;
 
-var toolbox = Blockly.Xml.textToDom(menuXml);
+const toolbox = Blockly.Xml.textToDom(menuXml);
+const zoom = {
+  controls: true,
+  wheel: true,
+  startScale: 1.0,
+  maxScale: 3,
+  minScale: 0.3,
+  scaleSpeed: 1.2
+};
 
 const workspace = Blockly.inject('blocklyDiv', {
-    toolbox: toolbox,
-    media: 'media/',
-    toLocaleString: JA
+  toolbox: toolbox,
+  media: 'media/',
+  trashcan: true,
+  zoom: zoom,
 });
-
