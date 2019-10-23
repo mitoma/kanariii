ef2d0301bcb202400ce00db540d07f4639039577
ryo-mitoma <ryo-mitoma@cybozu.co.jp>
2019-10-23 13:11:07 +0900

    もろもろいい感じに

--- src/kintone-block.ts
+++ src/kintone-block.ts
@@ -1,29 +1,44 @@
-import * as Blockly from 'blockly/core';
+import Blockly from 'blockly/core';
 import 'blockly/javascript';
 
-export class ShowRecordBlock {
-    blockName(): string { return 'kinblock'; }
+interface KintoneBlocks {
+    readonly blockName: string;
+    readonly blockDefinition: object;
+    readonly jsGenerator: (block: any) => string;
+    menuElement(): HTMLElement;
+}
 
-    blockDefinition(): object {
-        return {
-            init: function () {
-                this.appendStatementInput("success")
-                    .setCheck(null)
-                    .appendField("コード");
-                this.setColour(60);
-                this.setTooltip("");
-                this.setHelpUrl("");
-            }
+abstract class KintoneBaseBlocks implements KintoneBlocks {
+    blockName: string;
+    blockDefinition: object;
+    jsGenerator: (block: any) => string;
+    menuElement(): HTMLElement {
+        let blockElement = document.createElement("block");
+        blockElement.setAttribute("type", this.blockName);
+        return blockElement;
+    }
+}
+
+export class ShowRecordBlock extends KintoneBaseBlocks {
+    blockName: string = 'kinblock';
+
+    blockDefinition: object = {
+        init: function () {
+            this.appendStatementInput("success")
+                .setCheck(null)
+                .appendField("コード");
+            this.setColour(60);
+            this.setTooltip("");
+            this.setHelpUrl("");
         }
     }
 
-    jsGenerator(): (block) => string {
-        return function (block): string {
-            let statements_success = Blockly.JavaScript.statementToCode(block, 'success');
-            return `kintone.events.on('app.record.index.show', function(event) {
+    jsGenerator: (block) => string = function (block): string {
+        let statements_success = Blockly.JavaScript.statementToCode(block, 'success');
+        return `
+kintone.events.on('app.record.index.show', function(event) {
 ${statements_success}
 };
 `;
-        };
     }
 }
