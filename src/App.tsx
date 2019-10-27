6fb4363a3cecba4e2e4eeefd367c1608a39e97ca
mitoma <mutetheradio@gmail.com>
2019-10-27 16:35:35 +0900

    モジュール名の変更＆デザインの軽微な調整

---
+++ src/App.tsx
@@ -0,0 +1,32 @@
+import { BlocklyUi } from "./BlocklyUi";
+import * as React from "react";
+
+type AppProps = {}
+
+type AppState = {
+    showBlocklyEditor: boolean;
+}
+
+export class App extends React.Component<AppProps, AppState> {
+
+    constructor(props: AppProps) {
+        super(props);
+        this.state = { showBlocklyEditor: false };
+
+        this.handleToggleEditor = this.handleToggleEditor.bind(this);
+    }
+
+    handleToggleEditor() {
+        this.setState({ showBlocklyEditor: !this.state.showBlocklyEditor });
+    }
+
+    render() {
+        return (
+            <React.Fragment>
+                <div>Kintone with Blockly</div>
+                <input type="button" value="Toggle Blockly Editor" onClick={this.handleToggleEditor}></input>
+                <BlocklyUi visible={this.state.showBlocklyEditor} handleToggleEditor={this.handleToggleEditor} />
+            </React.Fragment>
+        );
+    }
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,35 +1,5 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { BlocklyUi } from './ui';
-
-type AppProps = {}
-
-type AppState = {
-  showBlocklyEditor: boolean;
-}
-
-class App extends React.Component<AppProps, AppState> {
-
-  constructor(props: AppProps) {
-    super(props);
-    this.state = { showBlocklyEditor: false };
-
-    this.handleToggleEditor = this.handleToggleEditor.bind(this);
-  }
-
-  handleToggleEditor() {
-    this.setState({ showBlocklyEditor: !this.state.showBlocklyEditor });
-  }
-
-  render() {
-    return (
-      <React.Fragment>
-        <div>Kintone with Blockly</div>
-        <input type="button" value="Toggle Blockly Editor" onClick={this.handleToggleEditor}></input>
-        <BlocklyUi visible={this.state.showBlocklyEditor} handleToggleEditor={this.handleToggleEditor} />
-      </React.Fragment>
-    );
-  }
-}
+import { App } from './App';
 
 ReactDOM.render(<App />, document.getElementById('app'));
