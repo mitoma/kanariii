13bc355159c966ee50413612d8bf7de33299e3ea
mitoma <mutetheradio@gmail.com>
2019-10-28 23:27:07 +0900

    アプリにすでに設定されているデータをロードする。

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,15 @@ import * as ReactDOM from 'react-dom';
 import { App } from './App';
 
 document.addEventListener("DOMContentLoaded", function (loadedEvent) {
+    let sourceXml = '';
+    if (KintoneBlockly != null && KintoneBlockly.sourceXml != null) {
+        sourceXml = KintoneBlockly.sourceXml;
+    }
     const kintoneMenu = document.querySelector('.gaia-argoui-app-toolbar-menu');
     if (kintoneMenu != null) {
         const blocklyToggle = document.createElement('span');
         document.querySelector('.gaia-argoui-app-menu-settings').before(blocklyToggle);
-        ReactDOM.render(<App />, blocklyToggle);
+        ReactDOM.render(<App sourceXml={sourceXml} />, blocklyToggle);
     } else {
         console.log("hello");
     }
--- src/typing.d.ts
+++ src/typing.d.ts
@@ -7,3 +7,7 @@ declare module '*.css' {
     const classes: { readonly [key: string]: string };
     export default classes;
 }
+
+declare namespace KintoneBlockly {
+    const sourceXml: string;
+}
