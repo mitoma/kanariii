e0a0bbbf67734849fbbf23aa472dcc942c3ef538
ryo-mitoma <ryo-mitoma@cybozu.co.jp>
2019-10-28 22:18:23 +0900

    kintone 上で動作するように修正した。

--- src/main.tsx
+++ src/main.tsx
@@ -2,4 +2,13 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { App } from './App';
 
-ReactDOM.render(<App />, document.getElementById('app'));
+document.addEventListener("DOMContentLoaded", function (loadedEvent) {
+    const kintoneMenu = document.querySelector('.gaia-argoui-app-toolbar-menu');
+    if (kintoneMenu != null) {
+        const blocklyToggle = document.createElement('span');
+        document.querySelector('.gaia-argoui-app-menu-settings').before(blocklyToggle);
+        ReactDOM.render(<App />, blocklyToggle);
+    } else {
+        console.log("hello");
+    }
+});
