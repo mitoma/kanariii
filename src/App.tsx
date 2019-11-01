987390ca870c5dfb79e86c42f5c89333e0b96eeb
mitoma <mutetheradio@gmail.com>
2019-11-01 23:49:07 +0900

    コンポーネントの行き場を整理

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,8 @@
 import { BlocklyUi } from "./BlocklyUi";
 import * as React from "react";
 import { Field } from "./schema/Field";
-import { Typography, Dialog, IconButton, AppBar, Toolbar, Box } from "@material-ui/core";
+import { Dialog, IconButton } from "@material-ui/core";
 import BuildIcon from '@material-ui/icons/Build';
-import CloseIcon from '@material-ui/icons/Close';
-import ArrowUpwardIcon from '@material-ui/icons/ArrowUpward';
-import ArrowDownwardIcon from '@material-ui/icons/ArrowDownward';
-import SaveAltIcon from '@material-ui/icons/SaveAlt';
-
 
 type AppProps = {
     sourceXml: string;
@@ -24,13 +19,8 @@ export class App extends React.Component<AppProps, AppState> {
         super(props);
         this.state = { showBlocklyEditor: false };
 
-        this.handleToggleEditor = this.handleToggleEditor.bind(this);
         this.handleOpenEditor = this.handleOpenEditor.bind(this);
         this.handleCloseEditor = this.handleCloseEditor.bind(this);
-    }
-
-    handleToggleEditor() {
-        this.setState({ showBlocklyEditor: !this.state.showBlocklyEditor });
     }
 
     handleOpenEditor() {
@@ -47,29 +37,14 @@ export class App extends React.Component<AppProps, AppState> {
                 <IconButton edge="start" color="inherit" onClick={this.handleOpenEditor} aria-label="open blockly">
                     <BuildIcon >open blockly</BuildIcon>
                 </IconButton>
-                <Dialog maxWidth={'xl'} open={this.state.showBlocklyEditor} onClose={this.handleCloseEditor} style={{ zIndex: 200 }}>
-                    <AppBar style={{ position: 'relative' }}>
-                        <Toolbar>
-                            <IconButton edge="start" color="inherit" onClick={this.handleCloseEditor} aria-label="close">
-                                <CloseIcon />
-                            </IconButton>
-                            <Box flex={1} marginLeft={4}>
-                                <Typography variant="h6">KintoneBlockly</Typography>
-                            </Box>
-                            <IconButton color="inherit" aria-label="upload code">
-                                <ArrowUpwardIcon />
-                            </IconButton>
-                            <IconButton color="inherit" aria-label="download code">
-                                <ArrowDownwardIcon />
-                            </IconButton>
-                            <IconButton color="inherit" aria-label="deploy code">
-                                <SaveAltIcon />
-                            </IconButton>
-                        </Toolbar>
-                    </AppBar>
+                <Dialog
+                    maxWidth={'xl'}
+                    open={this.state.showBlocklyEditor}
+                    onClose={this.handleCloseEditor}
+                    style={{ zIndex: 200 }}>
                     <BlocklyUi
                         visible={true}
-                        handleToggleEditor={this.handleToggleEditor}
+                        handleCloseEditor={this.handleCloseEditor}
                         sourceXml={this.props.sourceXml}
                         fields={this.props.fields}
                     />
