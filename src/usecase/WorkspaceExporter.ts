78382ad83ff6ab026e81333b5ca3566803ca05ed
mitoma <mutetheradio@gmail.com>
2019-11-14 23:47:28 +0900

    履歴を保存する機能を追加

--- src/usecase/WorkspaceExporter.ts
+++ src/usecase/WorkspaceExporter.ts
@@ -1,6 +1,7 @@
 import * as Blockly from 'blockly';
 import 'blockly/javascript';
 import { CustomizeJsUpdater } from '../CustomizeJsUpdater';
+import { Revision } from '../history/Revision';
 
 export class WorkspaceExporter {
   exportXml(workspace: Blockly.Workspace) {
@@ -12,12 +13,13 @@ export class WorkspaceExporter {
     this.download(filename, blob);
   }
 
-  exportJavaScript(workspace: Blockly.Workspace) {
+  exportJavaScript(workspace: Blockly.Workspace, revisions: Revision[]) {
     const filename = 'kintone-blockly-app.js';
     const jsCode = new CustomizeJsUpdater().generateCode(
       Blockly.Xml.domToText(Blockly.Xml.workspaceToDom(workspace)),
       // @ts-ignore Blockly.JavaScript は型定義がまだ未対応。
       Blockly.JavaScript.workspaceToCode(workspace),
+      revisions,
     );
     const blob = new Blob([jsCode], { type: 'application/javascript' });
     this.download(filename, blob);
