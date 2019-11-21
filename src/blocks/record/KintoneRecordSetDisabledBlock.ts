74580a89fa29fa857eccb37137e79277f5d3ad11
mitoma <mutetheradio@gmail.com>
2019-11-21 16:34:27 +0900

    ディレクトリの移動

--- src/blocks/record/KintoneRecordSetDisabledBlock.ts
+++ src/blocks/record/KintoneRecordSetDisabledBlock.ts
@@ -1,12 +1,12 @@
-import { KintoneBlock } from './KintoneBlock';
+import { KintoneBlock } from '../KintoneBlock';
 import * as Blockly from 'blockly';
 import 'blockly/javascript';
-import { Field } from '../schema/Field';
-import { BlockColors, appendShadowText } from './block-definition-util';
+import { Field } from '../../schema/Field';
+import { BlockColors } from '../block-definition-util';
 
-export class KintoneRecordSetValueBlock implements KintoneBlock {
+export class KintoneRecordSetDisabledBlock implements KintoneBlock {
   constructor(private fields: Field[]) {}
-  blockName = 'kintone_app_record_set_value';
+  blockName = 'kintone_app_record_set_disabled';
   blockDefinition(): object {
     const fieldsDropdown = this.fields.map(f => {
       return [f.label, f.var];
@@ -14,7 +14,7 @@ export class KintoneRecordSetValueBlock implements KintoneBlock {
     return {
       init: function() {
         const jsonDefinition = {
-          message0: '%{BKY_KINTONE_APP_RECORD_SET_VALUE_MSG}',
+          message0: '%{BKY_KINTONE_APP_RECORD_SET_DISABLED_MSG}',
           args0: [
             {
               type: 'field_dropdown',
@@ -22,15 +22,19 @@ export class KintoneRecordSetValueBlock implements KintoneBlock {
               options: fieldsDropdown,
             },
             {
-              type: 'input_value',
-              name: 'TEXT',
+              type: 'field_dropdown',
+              name: 'disabled',
+              options: [
+                ['%{BKY_KINTONE_APP_RECORD_SET_DISABLED_FALSE_MSG}', 'false'],
+                ['%{BKY_KINTONE_APP_RECORD_SET_DISABLED_TRUE_MSG}', 'true'],
+              ],
             },
           ],
           inputsInline: true,
           previousStatement: 'Action',
           nextStatement: 'Action',
           colour: BlockColors.KINTONE,
-          tooltip: 'フィールドの値をセットします',
+          tooltip: 'フィールドの編集可/不可を設定します',
           helpUrl: '',
         };
         this.jsonInit(jsonDefinition);
@@ -41,21 +45,15 @@ export class KintoneRecordSetValueBlock implements KintoneBlock {
   jsGenerator(): (block: Blockly.Block) => string {
     return function(block): string {
       var fieldCode = JSON.stringify(block.getFieldValue('field_code'));
-      const inputValue =
-        // @ts-ignore
-        Blockly.JavaScript.valueToCode(
-          block,
-          'TEXT',
-          // @ts-ignore
-          Blockly.JavaScript.ORDER_ATOMIC,
-        );
-      return `event['record'][${fieldCode}]['value'] = ${inputValue};`;
+      // disabled は文字列で "true", "false" が入っているので stringify 不要
+      var disabled = block.getFieldValue('disabled');
+      return `event['record'][${fieldCode}]['disabled'] = ${disabled};`;
     };
   }
 
   menuElement(): Element {
     let blockElement = document.createElement('block');
     blockElement.setAttribute('type', this.blockName);
-    return appendShadowText(blockElement, '');
+    return blockElement;
   }
 }
