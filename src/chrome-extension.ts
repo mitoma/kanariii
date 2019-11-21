d1e1bf4ccd11a9b3ebe2ee64602394dfd3dc35f5
mitoma <mutetheradio@gmail.com>
2019-11-22 01:44:38 +0900

    メディアを拡張の中で持つようにした。

--- src/chrome-extension.ts
+++ src/chrome-extension.ts
@@ -1,9 +1,16 @@
 function injectScript(file: string) {
   const bodyTag = document.getElementsByTagName('body')[0];
-  const scriptTag = document.createElement('script');
-  scriptTag.setAttribute('type', 'text/javascript');
-  scriptTag.setAttribute('src', file);
-  return bodyTag.appendChild(scriptTag);
+  const mediaScript = document.createElement('script');
+  mediaScript.setAttribute('type', 'text/javascript');
+  mediaScript.innerText = `kintoneBlocklyMediaUrl = ${JSON.stringify(
+    chrome.extension.getURL('/media/'),
+  )};`;
+  bodyTag.appendChild(mediaScript);
+
+  const mainScript = document.createElement('script');
+  mainScript.setAttribute('type', 'text/javascript');
+  mainScript.setAttribute('src', file);
+  bodyTag.appendChild(mainScript);
 }
 
 injectScript(chrome.extension.getURL('/kintone-blockly.js'));
