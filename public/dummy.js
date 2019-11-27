c31b9a76f03153ffb4bd251725160a5b6212a3a4
ryo-mitoma <ryo-mitoma@cybozu.co.jp>
2019-11-27 16:59:04 +0900

    プラグインじゃない状態で開発するためのコード

---
+++ public/dummy.js
@@ -0,0 +1,6 @@
+var KintoneBlockly = KintoneBlockly || {};
+KintoneBlockly.sourceXml =
+  KintoneBlockly.sourceXml ||
+  '<xml xmlns="https://developers.google.com/blockly/xml"></xml>';
+KintoneBlockly.revisions = KintoneBlockly.revisions || [];
+KintoneBlockly.mediaUrl = null;
