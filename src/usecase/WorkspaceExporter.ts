972124602f7f862b8c8accbced3211552b927699
mitoma <mutetheradio@gmail.com>
2019-12-03 22:57:24 +0900

    リファクタリング！

--- src/usecase/WorkspaceExporter.ts
+++ src/usecase/WorkspaceExporter.ts
@@ -1,24 +1,19 @@
 import * as Blockly from 'blockly';
-import 'blockly/javascript';
-import { CustomizeJsUpdater } from './CustomizeJsUpdater';
 import { Revision } from '../history/Revision';
+import { generateJavaScriptWithMetadata, generateXml } from './CodeGenerator';
 
 export class WorkspaceExporter {
   exportXml(workspace: Blockly.Workspace) {
     const filename = 'kanariii.xml';
-    const xmlData = Blockly.Xml.domToText(
-      Blockly.Xml.workspaceToDom(workspace),
-    );
+    const xmlData = generateXml(workspace);
     const blob = new Blob([xmlData], { type: 'application/xml' });
     this.download(filename, blob);
   }
 
   exportJavaScript(workspace: Blockly.Workspace, revisions: Revision[]) {
     const filename = 'kanariii-app.js';
-    const jsCode = new CustomizeJsUpdater().generateCode(
-      Blockly.Xml.domToText(Blockly.Xml.workspaceToDom(workspace)),
-      // @ts-ignore Blockly.JavaScript は型定義がまだ未対応。
-      Blockly.JavaScript.workspaceToCode(workspace),
+    const jsCode = generateJavaScriptWithMetadata(
+      workspace,
       'empty message',
       revisions,
     );
