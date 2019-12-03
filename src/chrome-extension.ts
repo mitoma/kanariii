976b5a7eda5d50f548de4580bbfbe1424f8ee193
mitoma <mutetheradio@gmail.com>
2019-12-03 09:28:29 +0900

    KintoneBlockly -> KanariIi

--- src/chrome-extension.ts
+++ src/chrome-extension.ts
@@ -3,12 +3,10 @@ function injectScript(file: string) {
   const mediaScript = document.createElement('script');
   mediaScript.setAttribute('type', 'text/javascript');
   mediaScript.innerText = `
-  var KintoneBlockly = KintoneBlockly || {};
-  KintoneBlockly.sourceXml = KintoneBlockly.sourceXml || '<xml xmlns="https://developers.google.com/blockly/xml"></xml>';
-  KintoneBlockly.revisions = KintoneBlockly.revisions || [];
-  KintoneBlockly.mediaUrl = ${JSON.stringify(
-    chrome.extension.getURL('/media/'),
-  )};`;
+  var KanariIi = KanariIi || {};
+  KanariIi.sourceXml = KanariIi.sourceXml || '<xml xmlns="https://developers.google.com/blockly/xml"></xml>';
+  KanariIi.revisions = KanariIi.revisions || [];
+  KanariIi.mediaUrl = ${JSON.stringify(chrome.extension.getURL('/media/'))};`;
   bodyTag.appendChild(mediaScript);
 
   const mainScript = document.createElement('script');
