f0ab21da1e792c6a18043003f9aef36c47188a9a
mitoma <mutetheradio@gmail.com>
2019-12-05 00:10:59 +0900

    アイコンを作った

---
+++ src/view/SvgKanariIiIcon.tsx
@@ -0,0 +1,36 @@
+import * as React from 'react';
+
+const SvgKanariIiIcon = props => (
+  <svg
+    viewBox="0 0 252.008 252.315"
+    fill="none"
+    strokeLinecap="square"
+    strokeMiterlimit={10}
+    width="1em"
+    height="1em"
+    {...props}>
+    <clipPath id="kanariii-icon_svg__a">
+      <path d="M0 0h252.008v252.315H0V0z" />
+    </clipPath>
+    <g clipPath="url(#kanariii-icon_svg__a)">
+      <path fill="none" d="M0 0h252.008v252.315H0z" />
+      <path
+        fill="#900"
+        d="M7.437 101.03a5.408 5.408 0 015.408-5.408h195.262a5.408 5.408 0 015.409 5.408v50.254a5.408 5.408 0 01-5.409 5.409H12.845a5.408 5.408 0 01-5.408-5.409z"
+        fillRule="evenodd"
+      />
+      <path
+        fill="#3d85c6"
+        d="M7.437 30.34a5.408 5.408 0 015.408-5.408h140.05a5.408 5.408 0 015.408 5.408v50.254a5.408 5.408 0 01-5.408 5.409H12.845a5.408 5.408 0 01-5.408-5.409z"
+        fillRule="evenodd"
+      />
+      <path
+        fill="#f1c232"
+        d="M7.437 171.72a5.408 5.408 0 015.408-5.408h226.317a5.408 5.408 0 015.409 5.409v50.254a5.408 5.408 0 01-5.409 5.408H12.845a5.408 5.408 0 01-5.408-5.408zM166.911 30.34a5.408 5.408 0 015.408-5.408h51.325a5.408 5.408 0 015.409 5.408v50.254a5.408 5.408 0 01-5.409 5.409H172.32a5.408 5.408 0 01-5.408-5.409z"
+        fillRule="evenodd"
+      />
+    </g>
+  </svg>
+);
+
+export default SvgKanariIiIcon;
