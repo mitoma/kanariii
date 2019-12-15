9cc22b37c02308bbbfddc84834adeddbc841ddec
mitoma <mutetheradio@gmail.com>
2019-12-16 00:00:49 +0900

    add test.

--- src/blocks/blockly.test.ts
+++ src/blocks/blockly.test.ts
@@ -1,7 +1,9 @@
 import * as TestHelper from '../TestHelper';
+import { loadEnLocale } from '../msg/all';
 
-test('test text_print block', () => {
-  const code: string = TestHelper.xmlToCode(`
+describe('blockly default block', () => {
+  test('test text_print block', () => {
+    const code: string = TestHelper.xmlToCode(`
       <block type="text_print">
         <value name="TEXT">
           <shadow type="text">
@@ -9,11 +11,63 @@ test('test text_print block', () => {
           </shadow>
         </value>
       </block>`);
-  expect(code).toBe(TestHelper.format("window.alert('hello')"));
+    expect(code).toBe(TestHelper.format("window.alert('hello')"));
+  });
 });
 
-test('test debugger block', () => {
-  TestHelper.loadKintoneBlocks();
-  const code: string = TestHelper.xmlToCode(`<block type="debugger"></block>`);
-  expect(code).toBe(TestHelper.format('debugger'));
+describe('debug block', () => {
+  beforeEach(() => {
+    loadEnLocale();
+    TestHelper.loadKintoneBlocks();
+  });
+
+  test('test debugger block', () => {
+    const code: string = TestHelper.xmlToCode(
+      `<block type="debugger"></block>`,
+    );
+    expect(code).toBe(TestHelper.format('debugger'));
+  });
+
+  test('test console block', () => {
+    const code: string = TestHelper.xmlToCode(`
+      <block type="console_log">
+        <value name="TEXT">
+          <shadow type="text">
+            <field name="TEXT">debug message</field>
+          </shadow>
+        </value>
+      </block>`);
+    expect(code).toBe(TestHelper.format('console.log("debug message")'));
+  });
+});
+
+describe('event block', () => {
+  beforeEach(() => {
+    loadEnLocale();
+    TestHelper.loadKintoneBlocks();
+  });
+
+  test('kintone_event_app_record_index block', () => {
+    const code: string = TestHelper.xmlToCode(`
+      <block type="kintone_event_app_record_create">
+        <field name="event_type">app.record.create.show</field>
+        <statement name="event_callback">
+          <block type="console_log">
+            <value name="TEXT">
+              <shadow type="text">
+                <field name="TEXT">debug message</field>
+              </shadow>
+            </value>
+          </block>
+        </statement>
+      </block>`);
+    expect(code).toBe(
+      TestHelper.format(`
+        kintone.events.on("app.record.create.show", function(event) {
+          console.log("debug message");
+          return event;
+        });
+    `),
+    );
+  });
 });
